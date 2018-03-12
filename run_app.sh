8fbef0c52b7e4d8aeb1e0cb625e4937223f71edb
Mário Hunka <hunka.mario@gmail.com>
2018-03-12 19:23:42 +0100

    initial commit

---
+++ run_app.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DB_FILE='db.sqlite3'
+if [ ! -z $1 ]; then
+    rm ${DB_FILE}
+fi
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "Creating superuser."
+python3 manage.py createsuperuser --username maroshmka --email maros@hmka.sk
+
+python3 manage.py runserver 0:8003
